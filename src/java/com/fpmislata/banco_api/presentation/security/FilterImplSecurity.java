b2304d67344a475b816316420bc389528004d77a
alumno <alumno@2A5PC24>
2015-11-09 13:06:51 +0100

    CREATED BUILD FOLDER

--- src/java/com/fpmislata/banco_api/presentation/security/FilterImplSecurity.java
+++ src/java/com/fpmislata/banco_api/presentation/security/FilterImplSecurity.java
@@ -12,18 +12,17 @@ public class FilterImplSecurity implements Filter {
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
-
+        
     }
- 
+
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
- 
-
+        
     }
 
     @Override
     public void destroy() {
-
+        
     }
 
 }
